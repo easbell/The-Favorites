8100aa569020438a00f9a280f68e6c3054054f25
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-03-19 14:45:13 -0600

    Add movie data cleaner

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import { fetchData } from '../utils/fetch'
+import { key } from '../utils/apiKEY';
+import { cleanMovieData } from '../utils/helpers';
 
 class App extends Component {
+
   componentDidMount = () => {
     this.fetchMovies()
   }
   
   fetchMovies = async () => {
-    const url = `https://api.themoviedb.org/`
-    const allMovies = await fetchData(url)
+    const url = `https://api.themoviedb.org/3/trending/all/day?api_key=${key}`;
+    const allMovies = await fetchData(url);
+    const cleanData = cleanMovieData(allMovies);
+    console.log(cleanData);
   }
 
   render() {
---
+++ src/utils/helpers.js
@@ -0,0 +1,11 @@
+export const cleanMovieData = (movies) => {
+  return movies.results.map(movie => {
+    return ({
+        title: movie.title,
+        rating: movie.vote_average,
+        id: movie.id,
+        releaseDate: movie.release_date,
+        synopsis: movie.overview
+    })
+  })
+}
