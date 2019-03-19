6ecc4939ef58bf989c65aea68b4685df576207a5
Elizabeth Asbell <e.asbell8@gmail.com>
2019-03-19 14:24:05 -0600

    Create fetchMovies function.

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
+import { fetchData } from '../utils/fetch'
 
 class App extends Component {
+  componentDidMount = () => {
+    this.fetchMovies()
+  }
+  
+  fetchMovies = async () => {
+    const url = `https://api.themoviedb.org/`
+    const allMovies = await fetchData(url)
+  }
+
   render() {
     return (
       <div className="App">
