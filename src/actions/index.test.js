e08421e8e99e856c3aab16e6310fc08aac0175fe
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-03-21 07:49:00 -0600

    Cleanup

--- src/actions/index.test.js
+++ src/actions/index.test.js
@@ -2,7 +2,7 @@ import * as actions from './index';
 import { mockMovies } from '../utils/mockData';
 
 describe('actions', () => {
-  it('should a return a type of ALL_ALL_MOVIES with all the movies', () => {
+  it('should a return a type of ADD_ALL_MOVIES with all the movies', () => {
     const expected = {
       type: 'ADD_ALL_MOVIES',
       movies: mockMovies
