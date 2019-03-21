acf2afa25683fc73215390d8e5c9e78c44d9899c
Elizabeth Asbell <e.asbell8@gmail.com>
2019-03-20 19:07:44 -0600

    Add sign in fetch call.

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -1,6 +1,6 @@
-export const fetchData = async (url) => {
+export const fetchData = async (url, options) => {
   try {
-    const response = await fetch(url)
+    const response = await fetch(url, options)
     const parsedData = response.json()
     return parsedData;
   } catch(error) {
