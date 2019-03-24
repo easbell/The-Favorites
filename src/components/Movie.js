3161267fc160dfa1da7237ad9618d3498a3c400b
Elizabeth Asbell <e.asbell8@gmail.com>
2019-03-24 15:28:56 -0600

    Add validateFavorite function.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,27 +1,62 @@
 import React, {Component} from 'react';
 import { fetchData } from '../utils/fetch';
+import { addFavorites } from '../actions';
 import { connect } from 'react-redux';
 
 export class Movie extends Component {
+  constructor() {
+    super()
+    this.state = {
+      message: ''
+    }
+  }
+
+  validateFavorite = async () => {
+    const { user_id, id } = this.props
+    const url = `http://localhost:3000/api/users/${user_id}/favorites`
+    if(user_id) {
+      const allFavorites = await fetchData(url)
+      const usersFavorites = allFavorites.data.filter(favorites => {
+        return favorites.id === user_id
+      })
+      usersFavorites.forEach(favorite => {
+        if(favorite.movie_id === id) {
+          console.log('alreay in favorites')
+        } else {
+          this.addFavorite()
+        }
+      })
+    }
+  }
+
   addFavorite = async (e) => {
     const { id, title, rating, user_id, posterImage, synopsis, releaseDate } = this.props
-    const url = "http://localhost:3000/api/users/favorites/new"
-    const movie = {
-      movie_id: id, 
-      user_id: user_id, 
-      title: title, 
-      poster_path: posterImage, 
-      release_date: releaseDate, 
-      vote_average: rating, 
-      overview: synopsis
-    }
-    const favorite = await fetchData(url, {
-      method: "POST", 
-      body: JSON.stringify(movie),
-      headers: {
-        "Content-Type": "application/json"
+    if(user_id) {
+      const url = "http://localhost:3000/api/users/favorites/new"
+      const movie = {
+        movie_id: id, 
+        user_id: user_id, 
+        title: title, 
+        poster_path: posterImage, 
+        release_date: releaseDate, 
+        vote_average: rating, 
+        overview: synopsis
       }
-    })
+      try {
+        await fetchData(url, {
+          method: "POST", 
+          body: JSON.stringify(movie),
+          headers: {
+            "Content-Type": "application/json"
+          }
+        })
+      } catch(error) {
+        console.log(error.message)
+      }
+    } else {
+      // need to render this to page
+      console.log('sorry, please log in')
+    }
   }
 
   render() {
@@ -38,7 +73,7 @@ export class Movie extends Component {
         {/* <p classsName="rating">{rating}</p> */}
         <img src={image} alt='movie poster'/> 
         {/* <img src='../utils/assets/not-favorite.png' alt='favorite-icon'/> */}
-        <button onClick={this.addFavorite}>Favorite</button>
+        <button onClick={this.validateFavorite}>Favorite</button>
       </div>
     )
   }
@@ -47,5 +82,5 @@ export class Movie extends Component {
 export const mapStateToProps = (state) => ({
   user_id: state.user.id
 })
-  
+
 export default connect(mapStateToProps)(Movie)
