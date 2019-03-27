b56f31c08b52c245751a39ecc90a0bb6199a7412
Edgar Eloy Munoz <edgareloymunoz@gmail.com>
2019-03-26 18:05:22 -0600

    Signin test

--- src/containers/SignIn/SignIn.test.js
+++ src/containers/SignIn/SignIn.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import SignIn from './SignIn'
+import {SignIn} from './SignIn'
 import { mapDispatchToProps } from './SignIn'
 import { shallow } from 'enzyme'
 
@@ -35,6 +35,29 @@ describe('SignIn', () => {
 
       mapDispatchToProps(dispatch).addMessage()
       expect(dispatch.mock.calls[0][0]).toEqual({ type: 'ADD_MESSAGE' })
+    })
+
+    it('should call addMessage when a sign attempt is made', async () => {
+
+      const mockFn = jest.fn()
+
+      const wrapper = shallow(
+        <SignIn addMessage={mockFn}/>
+      )
+
+      const event = { preventDefault: () => {} }
+      jest.spyOn(event, 'preventDefault')
+      const mockResponse = 'success'
+
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        ok: true,
+        status: 200,
+        json: () => Promise.resolve(mockResponse)
+      }))
+
+      await wrapper.instance().handleSignIn(event)
+
+      expect(mockFn).toHaveBeenCalled()
     })
   })
 })
